05f03035570c861d7747dea5891614678346cc63
Jelmar <33655959+JelmarNL@users.noreply.github.com>
2017-11-14 19:48:16 +0100

    Update Main.java

--- me/JelmarNL/Eggplosives/Main.java
+++ me/JelmarNL/Eggplosives/Main.java
@@ -1,6 +1,7 @@
 package me.JelmarNL.Eggplosives;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
@@ -9,18 +10,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	
-	PluginManager pm = Bukkit.getPluginManager();
+	public PluginManager pm = Bukkit.getPluginManager();
+	public FileConfiguration config = getConfig();
 	
 	@Override
 	public void onEnable() {
 		pm.registerEvents(this, this);
+		config.addDefault("eggplosion-size", 3);
+		config.addDefault("fire-eggplosion", true);
+		config.options().copyDefaults(true);
+		saveConfig();
+		getLogger().info("Eggplosives has been enabled ENJOY OR DESTROY!");
 	}
 	
 	
 	
 	@Override
 	public void onDisable() {
-		
+		getLogger().info("Eggplosives has been disabled!");
 	}
 	
 	
@@ -28,7 +35,7 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onInteract(PlayerEggThrowEvent event) {
 		event.setHatching(false);
-		event.getEgg().getLocation().getWorld().createExplosion(event.getEgg().getLocation(), 3, false);
+		event.getEgg().getLocation().getWorld().createExplosion(event.getEgg().getLocation(), config.getLong("eggplosion-size"), config.getBoolean("fire-eggplosion"));
 	}
 	
 	
